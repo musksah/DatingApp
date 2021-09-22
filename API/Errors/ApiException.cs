eea9be2ac659282ff638b9550024b51c515d87f9
Sebastian Andres Huerfano Rodriguez <sebatian.huerfano@growdata.com.co>
2021-09-21 22:25:16 -0500

    End of section 7

---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Errors
+{
+    public class ApiException
+    {
+        public ApiException(int statuCode, string message = null, string details = null)
+        {
+            StatuCode = statuCode;
+            Message = message;
+            Details = details;
+        }
+
+        public int StatuCode { get; set; }
+        public string Message { get; set; }
+        public string Details { get; set; }
+    }
+}
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -11,24 +11,21 @@ import { AccountService } from '../_services/account.service';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-  model:any = {}
+  model: any = {}
 
-  constructor(public accountService: AccountService, private router: Router, 
+  constructor(public accountService: AccountService, private router: Router,
     private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 
-  login(){
-    this.accountService.login(this.model).subscribe(response =>{
-      this.router.navigateByUrl('/members')   
-    }, error => {
-      this.toastr.error(error.error);
-      console.log(error);
+  login() {
+    this.accountService.login(this.model).subscribe(response => {
+      this.router.navigateByUrl('/members')
     })
   }
 
-  logout(){
+  logout() {
     this.accountService.logout();
     this.router.navigateByUrl('/');
   }
